e334aecf7b4983a382d0fc1a1b754983bbfcccff
ChalaniWeerarthna <35931060+ChalaniWeerarthna@users.noreply.github.com>
2020-12-30 18:05:54 +0530

    Texts and shapes

---
+++ chaptor4.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+img = np.zeros((512,512,3),np.uint8)
+#print(img)
+#img[:] =255,0,255
+
+cv2.line(img,(0,0),(512,512),(0,255,0),3)
+cv2.rectangle(img,(0,0),(250,350),(0,0,255),cv2.FILLED)
+cv2.circle(img,(400,50),30,(255,255,0),3)
+cv2.putText(img,"OPENCV",(300,500),cv2.FONT_HERSHEY_COMPLEX,1,(255,0,255),3)
+cv2.imshow("Image",img)
+cv2.waitKey(0)

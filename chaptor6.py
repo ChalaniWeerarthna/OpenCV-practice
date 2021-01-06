bd321716baf3e3a0a6b09ca9c1c38a370a42d82b
ChalaniWeerarthna <35931060+ChalaniWeerarthna@users.noreply.github.com>
2021-01-06 13:06:16 +0530

    concat images updated

--- chaptor6.py
+++ chaptor6.py
@@ -2,9 +2,7 @@ import cv2
 import numpy as np
 
 
-def stackImages(imgArray,scale):
-    sizeW= imgArray[0][0].shape[1]
-    sizeH = imgArray[0][0].shape[0]
+def stackImages(scale,imgArray):
     rows = len(imgArray)
     cols = len(imgArray[0])
     rowsAvailable = isinstance(imgArray[0], list)
@@ -13,33 +11,38 @@ def stackImages(imgArray,scale):
     if rowsAvailable:
         for x in range ( 0, rows):
             for y in range(0, cols):
-                imgArray[x][y] = cv2.resize(imgArray[x][y], (sizeW, sizeH), None, scale, scale)
+                if imgArray[x][y].shape[:2] == imgArray[0][0].shape [:2]:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (0, 0), None, scale, scale)
+                else:
+                    imgArray[x][y] = cv2.resize(imgArray[x][y], (imgArray[0][0].shape[1], imgArray[0][0].shape[0]), None, scale, scale)
                 if len(imgArray[x][y].shape) == 2: imgArray[x][y]= cv2.cvtColor( imgArray[x][y], cv2.COLOR_GRAY2BGR)
         imageBlank = np.zeros((height, width, 3), np.uint8)
         hor = [imageBlank]*rows
         hor_con = [imageBlank]*rows
         for x in range(0, rows):
             hor[x] = np.hstack(imgArray[x])
-            hor_con[x] = np.concatenate(imgArray[x])
         ver = np.vstack(hor)
-        ver_con = np.concatenate(hor)
     else:
         for x in range(0, rows):
-            imgArray[x] = cv2.resize(imgArray[x], (sizeW, sizeH), None, scale, scale)
+            if imgArray[x].shape[:2] == imgArray[0].shape[:2]:
+                imgArray[x] = cv2.resize(imgArray[x], (0, 0), None, scale, scale)
+            else:
+                imgArray[x] = cv2.resize(imgArray[x], (imgArray[0].shape[1], imgArray[0].shape[0]), None,scale, scale)
             if len(imgArray[x].shape) == 2: imgArray[x] = cv2.cvtColor(imgArray[x], cv2.COLOR_GRAY2BGR)
         hor= np.hstack(imgArray)
-        hor_con= np.concatenate(imgArray)
         ver = hor
     return ver
 
-img = cv2.imread("Resources/lena.png")
+img = cv2.imread('Resources/lena.png')
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-imgStack =stackImages(0.5,([img,img,img]))
+imgStack = stackImages(0.5,([img,imgGray,img],[img,img,img]))
 
-#imgHor = np.hstack((img,img))
-#imgVer = np.vstack((img,img))
-
-#cv2.imshow("Horizontal",imgHor)
-#cv2.imshow("Vertical",imgVer)
+# imgHor = np.hstack((img,img))
+# imgVer = np.vstack((img,img))
+#
+# cv2.imshow("Horizontal",imgHor)
+# cv2.imshow("Vertical",imgVer)
 cv2.imshow("ImageStack",imgStack)
+
 cv2.waitKey(0)
